f515cd70da0456f5d969ce75fc13d789cf0e4b34
Ayoub-Zennouh <ayoub.zennouh@alumni.univ-avignon.fr>
2022-09-18 02:56:17 +0200

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 import './App.css';
-
+import Login from './components/login/Login';
+import {
+  Route,
+  Routes
+} from 'react-router-dom'
+import Signup from './components/signup/Signup';
+import Dashboard from './components/dashboard/dashboard';
 function App() {
   return (
+
     <div className="App">
-      <div className='d-flex flex-row vh-100 vw-100'>
-        <div className='vh-100 w-25 bg-primary'></div>
-        <div className='vh-100 w-75 d-flex flex-column justify-content-center right-grid'>
-          <form className='w-50 mx-auto d-flex flex-column align-items-center'>
-            <h1 className='mb-5'>Sign in</h1>
-            <div class="mb-3 w-50">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='E-mail'></input>
-            </div>
-            <div class="mb-3 w-50">
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder='Password'></input>
-            </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                <label class="form-check-label" for="exampleCheck1">Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-success w-50">Submit</button>
-            <button type="submit" class="btn btn-primary mt-3 w-50">Sign up</button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  </div>
   );
 }
 
