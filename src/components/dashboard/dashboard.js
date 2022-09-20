2ddce9750f7e84bbb49853dc7a51c00dd96173a3
Abdelhakim RASFI <hakim.rasfi@gmail.com>
2022-09-20 23:35:50 +0200

    menu component

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,14 +2,73 @@ import React from 'react';
 import './dashboard.css';
 import { BsSearch } from "react-icons/bs";
 import { GiSettingsKnobs } from "react-icons/gi"
-
+import { Sidenav, Nav } from 'rsuite';
+import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
+import GroupIcon from '@rsuite/icons/legacy/Group';
+import MagicIcon from '@rsuite/icons/legacy/Magic';
+import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 
 import JourneyCard from '../journey-card/JourneyCard'
 import GoogleMapReact from 'google-map-react';
 import Card from '../card/card';
 
+const styles = {
+  display: 'inline-table',
+  paddingTop: 40,
+};
+
+
+const CustomSidenav = ({ appearance, openKeys, expanded, onOpenChange, onExpand, ...navProps }) => {
+  return (
+    <div style={styles}>
+      <Sidenav
+        appearance={appearance}
+        expanded={expanded}
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+      >
+        <Sidenav.Body>
+          <Nav {...navProps}>
+            <Nav.Item eventKey="1"  icon={<DashboardIcon />}>
+              Dashboard
+            </Nav.Item>
+            <Nav.Item eventKey="2" icon={<GroupIcon />}>
+              User Group
+            </Nav.Item>
+
+            <Nav.Item eventKey="3" icon={<MagicIcon />}>
+              User Group
+            </Nav.Item>
+            <Nav.Item eventKey="4" icon={<GearCircleIcon />}>
+              Applications
+            </Nav.Item>
+            <Nav.Item eventKey="5"  icon={<DashboardIcon />}>
+              Dashboard
+            </Nav.Item>
+            <Nav.Item eventKey="6" icon={<GroupIcon />}>
+              User Group
+            </Nav.Item>
+
+            <Nav.Item eventKey="7" icon={<MagicIcon />}>
+              User Group
+            </Nav.Item>
+            <Nav.Item eventKey="8" icon={<GearCircleIcon />}>
+              Applications
+            </Nav.Item>
+
+
+          </Nav>
+        </Sidenav.Body>
+      </Sidenav>
+    </div>
+  );
+};
 
 export default function Dashboard() {
+
+  const [activeKey, setActiveKey] = React.useState('1');
+  const [openKeys, setOpenKeys] = React.useState(['3', '4']);
+  const [expanded, setExpand] = React.useState(false);
 
   const defaultProps = {
     center: {
@@ -20,10 +79,18 @@ export default function Dashboard() {
   };
 
   return (
-    <div className='d-flex flex-row vh-100 vw-100 main'>
-      <div className='bg-primary left-grid'>
+    <div  className='d-flex flex-row vh-100 vw-100 main backgroundBlue'>
 
-      </div>
+      <CustomSidenav
+        activeKey={activeKey}
+        openKeys={openKeys}
+        onOpenChange={setOpenKeys}
+        onSelect={setActiveKey}
+        expanded={expanded}
+        onExpand={setExpand}
+        appearance="inverse"
+      />
+
       <div className='d-flex flex-row justify-content-center right-grid'>
         <div className='center-left bg-transparent'>
 
@@ -37,10 +104,10 @@ export default function Dashboard() {
           </div>
           <p className='p-3 py-0 mb-0 text-start fs-5'>Active Orders</p>
           <div className='orders-list d-flex flex-column p-3'>
-            
-           <Card data={{orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival:'Casablanca, Morocco', driver: 'Abdelhakim Rasfi'}} />
-           <Card data={{orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival:'Casablanca, Morocco', driver: 'Abdelhakim Rasfi'}} />
-           <Card data={{orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival:'Casablanca, Morocco', driver: 'Abdelhakim Rasfi'}} />
+
+            <Card data={{ orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival: 'Casablanca, Morocco', driver: 'Abdelhakim Rasfi' }} />
+            <Card data={{ orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival: 'Casablanca, Morocco', driver: 'Abdelhakim Rasfi' }} />
+            <Card data={{ orderID: '#2342', product: 'Construction Materials', status: 'In Transit', dept_time: '06.05.2022 10:00 AM', arri_time: '06.06.2022 09:00 AM', departure: 'Khouribga, Morocco', arrival: 'Casablanca, Morocco', driver: 'Abdelhakim Rasfi' }} />
 
           </div>
         </div>
